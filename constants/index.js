d719bee7c77199a17b02e4368fbe9d2a7a1e327d
ozgurtutarr <ozgurtutarr@gmail.com>
2022-11-03 01:42:07 +0300

    first commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = '0xDDDfe439B7b732995787b61C3D08dc51a7e2DAA5';
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
