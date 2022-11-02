73818c308fe2af23fb88b5894c40f1ab9ca6d374
ozgurtutarr <ozgurtutarr@gmail.com>
2022-11-03 01:58:36 +0300

    my changes

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = '0xDDDfe439B7b732995787b61C3D08dc51a7e2DAA5';
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'https://mint-nft-collection.vercel.app/api/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
--- nftcollectiondapp/pages/index.js
+++ nftcollectiondapp/pages/index.js
@@ -368,9 +368,7 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className={styles.footer}>
-        Made with &#10084; by Crypto Devs
-      </footer>
+      <footer className={styles.footer}>0xfree</footer>
     </div>
   );
 }
